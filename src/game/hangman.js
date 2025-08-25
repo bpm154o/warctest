f55d342e149117009037f31fa9e06ed0d825f599
“bpm154o” <ethylc8h10@gmail.com>
2025-08-25 16:07:03 +0900

    [add]失敗可能回数と正誤判定を追加

--- src/game/hangman.js
+++ src/game/hangman.js
@@ -1,8 +1,11 @@
 const readline = require("readline");
+const { displayPartsToString } = require("typescript");
 
 const words = ["apple", "dog"];
 const word = words[Math.floor(Math.random() * words.length)].toLowerCase();
+
 let guesses = [];
+let attempts = 5; //失敗可能数
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,8 +19,28 @@ function getDisplayWord() {
     .join("");
 }
 
+function checkGameOver() {
+  // 勝ちのとき
+  if (!getDisplayWord().includes("_")) {
+    console.log("\n 勝ち！ 正解は " + word + "です");
+    rl.close();
+    return true;
+  }
+
+  // 負け、残り回数が0のとき
+  if (attempts <= 0) {
+    console.log("\n 負け… 正解は " + word + "です");
+    rl.close();
+    return true;
+  }
+
+  return false;
+}
+
 function askGuess() {
   console.log("\n単語: " + getDisplayWord());
+  console.log("残りミス回数：" + attempts);
+
   rl.question("1文字を入力してください：", (input) => {
     const guess = input.toLowerCase();
 
@@ -28,10 +51,19 @@ function askGuess() {
       console.log("その文字はすでに入力済みです");
     } else {
       guesses.push(guess);
+      // 入力が単語に含まれるかをチェック
+      if (word.includes(guess)) {
+        console.log("正解");
+      } else {
+        console.log("不正解");
+        attempts--;
+      }
     }
 
-    // 次の入力
-    askGuess();
+    // 勝ち負けの判定
+    if (!checkGameOver()) {
+      askGuess();
+    }
   });
 }
 
