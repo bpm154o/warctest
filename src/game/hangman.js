525953426fc9ddf7b130ba66d3357d6f139c6daa
“bpm154o” <ethylc8h10@gmail.com>
2025-08-25 15:35:36 +0900

    [add]1文字ずつ入力できるようにする

--- src/game/hangman.js
+++ src/game/hangman.js
@@ -1,7 +1,14 @@
+const readline = require("readline");
+
 const words = ["apple", "dog"];
 const word = words[Math.floor(Math.random() * words.length)].toLowerCase();
-
 let guesses = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 function getDisplayWord() {
   return word
     .split("")
@@ -9,5 +16,24 @@ function getDisplayWord() {
     .join("");
 }
 
+function askGuess() {
+  console.log("\n単語: " + getDisplayWord());
+  rl.question("1文字を入力してください：", (input) => {
+    const guess = input.toLowerCase();
+
+    // 入力チェック
+    if (!/^[a-z]$/.test(guess)) {
+      console.log("アルファベット1文字で入力してください");
+    } else if (guesses.includes(guess)) {
+      console.log("その文字はすでに入力済みです");
+    } else {
+      guesses.push(guess);
+    }
+
+    // 次の入力
+    askGuess();
+  });
+}
+
 console.log("単語あてゲーム");
-console.log(getDisplayWord());
+askGuess();
